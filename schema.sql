385c0a8d4702834c41b55a87bf9863d87a7c3152
Amr Ojjeh <amrojjeh@outlook.com>
2023-11-30 00:06:09 -0600

    Update schema

--- schema.sql
+++ schema.sql
@@ -3,12 +3,22 @@
 CREATE DATABASE IF NOT EXISTS BEApp;
 USE BEApp;
 
+-- Create users table
+CREATE TABLE users (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  creation DATETIME NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  pass_hash VARCHAR(255) NOT NULL
+);
+
+-- Create transactions table
 CREATE TABLE IF NOT EXISTS transactions (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	tdate DATETIME NOT NULL,
-	pdate DATETIME NOT NULL,
+	tdate DATETIME NOT NULL, -- actual date of transaction
+	pdate DATETIME NOT NULL, -- posted date of transaction
 	from_account VARCHAR(100) NOT NULL, -- account paying (could be a personal bank account)
 	to_account VARCHAR(100) NOT NULL, -- account earning (could be Amazon)
+	amount DECIMAL(10, 2) NOT NULL,
 	context TEXT NOT NULL,
 	tags VARCHAR(100) NOT NULL
 );
